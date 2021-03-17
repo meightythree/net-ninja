bdce2bd0107727344ca09b25fc030af4dc017363
palikollar <palkollar1995@gmail.com>
2021-03-17 20:25:58 +0100

    Dynamic values in templates

--- full-modern-react-tutorial/src/App.tsx
+++ full-modern-react-tutorial/src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog!";
+  const likes = 50;
+  const person = { age: 23, name: "Yoshi" };
+  const link = "https://google.com";
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{title}</h1>
+        <p>Likes {likes} times</p>
+        <p>
+          age: {person.age}, name: {person.name}
+        </p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
