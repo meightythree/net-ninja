367bdbe741cdbb67c16ece83a4fd1734b6230028
palikollar <palkollar1995@gmail.com>
2021-03-17 20:32:33 +0100

    Multiple Components

--- full-modern-react-tutorial/src/App.tsx
+++ full-modern-react-tutorial/src/App.tsx
@@ -1,20 +1,13 @@
 import "./App.css";
 
+import Navbar from "./Navbar";
+import Home from "./Home";
+
 function App() {
-  const title = "Welcome to the new blog!";
-  const likes = 50;
-  const person = { age: 23, name: "Yoshi" };
-  const link = "https://google.com";
   return (
     <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>Likes {likes} times</p>
-        <p>
-          age: {person.age}, name: {person.name}
-        </p>
-        <a href={link}>Google</a>
-      </div>
+      <Navbar />
+      <Home />
     </div>
   );
 }
